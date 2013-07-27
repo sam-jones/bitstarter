41ccf3c352fd6a04ffbc5451b3276fd49cd55c62
Sam Jones <sam.jones@gmail.com>
2013-07-27 19:12:57 +0000

    responsive

--- web.js
+++ web.js
@@ -7,7 +7,8 @@ app.get('/', function(request, response) {
   var outText = fs.readFileSync('index.html', 'utf8');  
   response.send(outText)});
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
